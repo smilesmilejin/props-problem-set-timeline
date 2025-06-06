6b5cacb6b47aebc320de7134a96378e0632695db
Xinshuang Jin <jinxinshuang@gmail.com>
2025-06-06 00:09:36 -0400

    Props Problem Set Timeline Official Solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,13 @@
 import './App.css';
-// Notice that our modified App component imports a JSON object, and declares the object in a variable named timelineData:
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
 
-
-// App Component
-//   The App component is a container component. It is responsible for:
-//   Holding the data about the timeline, which contains all timeline events.
-//   Rendering one instance of the Timeline component.
-//   Passing the timeline data to the Timeline component.
-
 function App() {
-  console.log('The value of timelineData is', timelineData);
-  console.log('We should use timelineData in our project somehow...');
 
-  // console.log('The value of timelineData.events is', timelineData.events);
   return (
-    // <div>
-    //   <Timeline timeline={timelineData.events}></Timeline>
-    // </div>
-      <section className="App-main">
+    <section className="App-main">
       <h1 className="App-header App-title">Ada Lovelace&apos;s Social Media Feed</h1>
-      {/* The key will be timelinedata when passed into TimeLine */}
-      <Timeline timelinedata={timelineData}></Timeline>
+      <Timeline data={timelineData}></Timeline>
     </section>
   );
 }
--- src/components/TimelineEvent.jsx
+++ src/components/TimelineEvent.jsx
@@ -2,32 +2,22 @@ import './TimelineEvent.css';
 import TimeStamp from './TimeStamp';
 import PropTypes from 'prop-types';
 
-// TimelineEvent Component
-// The TimelineEvent component is a presentational component. Its responsibilities include:
-//     Rendering the event's status message
-//     Rendering the event's author (person)
-//     Rendering the event's timestamp
-
-
 const TimelineEvent = (props) => {
-  console.log('########### props.timelineevent')
-  console.log(props.timelineevent); // {person: 'Adele Goldberg', status: 'In Smalltalk, everything happens somewhere else.', timestamp: '2023-05-18T22:12:03Z'}
-  const time = props.timelineevent.timestamp; // 2023-05-18T22:12:03Z
-  console.log(time);
-  return (
-    <ul>
-      <li>{props.timelineevent.person}</li>
-      <li>{props.timelineevent.status}</li>
-      <li><TimeStamp time={props.timelineevent.timestamp}></TimeStamp></li>
-    </ul>
-  )
-
+    return (
+        <section className="timeline-event">
+            <h3 className="event-person">{props.person}</h3>
+            <p className="event-status">{props.status}</p>
+            <p className="event-time">
+                <TimeStamp time={props.timeStamp}></TimeStamp>
+            </p>
+        </section>
+    );
 };
 
 TimelineEvent.propTypes = {
-  person:PropTypes.string.isRequired,
-  status:PropTypes.string.isRequired, 
-  timestamp: PropTypes.string.isRequired,
+    person: PropTypes.string.isRequired,
+    status: PropTypes.string.isRequired,
+    timeStamp: PropTypes.string.isRequired,
 };
 
 export default TimelineEvent;
